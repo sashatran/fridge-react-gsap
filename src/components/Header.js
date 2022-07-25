16f0d08720d9dbec86403d0013a099118f1a673a
unknown <shasha26789@gmail.com>
2022-07-25 14:43:04 -0700

    header, hero animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,38 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import gsap from "gsap";
+import { useEffect } from "react";
 
 export default function Header() {
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.from(".header", {
+      opacity: 0,
+      y: -50,
+      duration: 0.5,
+      stagger: 0.05,
+      ease: "sine.out",
+      delay: 0.2
+    });
+  });
+
   return (
     <header className="header">
       <div className="header-container">
-      <div className="logo-container">
-        <div className="logo-img"></div>
-        <p className="logo-copy">Fridge.</p>
-      </div>
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-list-item">Marketplace</li>
-          <li className="nav-list-item">Bacon</li>
-          <li className="nav-list-item">Connect Wallet</li>
-        </ul>
-      </nav>
-      <div className="mobile-nav-btn">
-      <FontAwesomeIcon className="mobile-nav-icon" icon={faChevronDown} />
-      </div>
+        <div className="logo-container">
+          <div className="logo-img"></div>
+          <p className="logo-copy">Fridge.</p>
+        </div>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-list-item">Marketplace</li>
+            <li className="nav-list-item">Bacon</li>
+            <li className="nav-list-item">Connect Wallet</li>
+          </ul>
+        </nav>
+        <div className="mobile-nav-btn">
+          <FontAwesomeIcon className="mobile-nav-icon" icon={faChevronDown} />
+        </div>
       </div>
     </header>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,25 @@
+import gsap from "gsap";
+import { useEffect } from "react";
+
 export default function Hero() {
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.from(".hero-container > *", {
+      opacity: 0,
+      y: 70,
+      duration: 0.7,
+      stagger: 0.05,
+      ease: "power4.out",
+      delay: 0.2
+    });
+  });
+
   return (
     <section className="hero-section">
       <div className="hero-container">
         <h1 className="hero-title">Your fridge but better.</h1>
         <p className="hero-copy">Bacon Driven. Open Economy.</p>
-        <div className="hero-cta-btn">
-            Join Us
-        </div>
+        <div className="hero-cta-btn">Join Us</div>
       </div>
     </section>
   );
